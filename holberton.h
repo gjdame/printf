f605a24db8d0b573932798d6c8e8d8212a10d151
gjdame <gregoryjdame@gmail.com>
2018-02-26 19:28:33 -0800

    more null checks

--- holberton.h
+++ holberton.h
@@ -20,5 +20,6 @@ char *print_c(va_list list);
 char *print_s(va_list list);
 char *print_i(va_list list);
 char *print_d(va_list list);
+char *print_p(va_list list);
 int return_position(const char *s, int n);
 #endif
