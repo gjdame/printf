c8e7580f61de7a02f83f1d834987cf3bf6fa947c
Amy <amy.tai0120@gmail.com>
2018-02-27 21:40:26 +0000

    fixed bloody octal

--- print_oct.c
+++ print_oct.c
@@ -9,18 +9,18 @@ char *print_oct(va_list list)
 {
 	char *output;
 	int i = 0;
-	int num_cp;
+	unsigned int num_cp;
 	int len;
-	unsigned int num = va_arg(list, unsigned int);
+	unsigned int num =  va_arg(list, unsigned int);
 
 	len = 0;
 	num_cp = num;
-
 	while (num_cp > 0)
 	{
 		num_cp /= 8;
 		len++;
 	}
+
 	output = malloc(sizeof(char) * len + 1);
 
 	if (output == NULL)
@@ -28,13 +28,15 @@ char *print_oct(va_list list)
 		free(output);
 		return (NULL);
 	}
-	while (len - 1>= 0)
+
+	while (len >= 0)
 	{
 		output[i] = ((num % 8) + '0');
 		num /= 8;
 		i++;
 		len--;
 	}
+
 	rev_string(output);
 	output[i] = '\0';
 
