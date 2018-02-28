989cff78beaedf24ee02d42998ec943aa9c84452
Amy <amy.tai0120@gmail.com>
2018-02-28 03:14:49 +0000

    oops comment prob

--- rot13.c
+++ rot13.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
+
 /**
- * rot13 = convert string to rot13
+ * rot13 - convert string to rot13
  * @list: argument passed
  * Return: pointer to rot13 string
  */
+
 char *rot13(va_list list)
 {
 	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
