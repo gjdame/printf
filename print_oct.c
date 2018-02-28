2944c328e1c4011f69d6143406399d3f3a3c14e0
gjdame <gregoryjdame@gmail.com>
2018-02-27 18:51:57 -0800

    fix octal

--- print_oct.c
+++ print_oct.c
@@ -7,30 +7,36 @@
 
 char *print_oct(va_list list)
 {
-	char output[1024];
-	char *out_ptr;
+	char *output;
 	int i = 0;
 	int len;
 	unsigned int num_cp = 0;
 	unsigned int num = va_arg(list, unsigned int);
 
-	out_ptr = output;
 	len = 0;
-
 	num_cp = num;
+
 	while (num_cp > 0)
 	{
 		num_cp /= 8;
 		len++;
 	}
 
+	output = malloc(sizeof(char) * len + 1);
+
+	if (output == NULL)
+	{
+		free(output);
+		return (NULL);
+	}
+
 	if (num == 0)
 	{
 		output[i] = '\0';
-		return (out_ptr);
+		return (output);
 	}
 
-	while (len > 0)
+	while (len >= 0)
 	{
 		output[i] = ((num % 8) + '0');
 		num /= 8;
@@ -41,5 +47,5 @@ char *print_oct(va_list list)
 	rev_string(output);
 	output[i] = '\0';
 
-	return (out_ptr);
+	return (output);
 }
